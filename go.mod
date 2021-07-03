733926e0b18ec3a2b9946f7fcd1bb2f7359dc636
David Joshua Reyes <davidjoshuareyes@gmail.com>
2021-07-03 12:18:18 -0400

    Refactor unit test and add more

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dajomareyes/serverus-snake
 
 go 1.16
 
-require github.com/BattlesnakeOfficial/rules v1.0.17 // indirect
+require (
+	github.com/BattlesnakeOfficial/rules v1.0.17 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- pkg/serverus_test/handleBoundaries_test.go
+++ pkg/serverus_test/handleBoundaries_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	. "github.com/dajomareyes/serverus-snake/pkg/serverus"
+	"github.com/stretchr/testify/assert"
 )
 
 var BOARD_DIMS = Board{Width: 10, Height: 10}
@@ -15,54 +16,55 @@ func setup(snakeHead Coord) (GameRequest, []string) {
 func TestWhenSnakeIsAtNorthernBoundary(t *testing.T) {
 	request, moves := setup(Coord{X: 5, Y: 10})
 	HandleBoundaries(request, &moves)
-
-	if Contains(moves, "up") {
-		t.Errorf("TestWhenSnakeIsAtNorthernBoundary failed, expected not to find 'up', got %v", moves)
-	} else {
-		t.Log("TestWhenSnakeIsAtNorthernBoundary passed")
-	}
+	expected := []string{"down", "left", "right"}
+	assert.Equal(t, expected, moves)
 }
 
 func TestWhenSnakeIsAtSourthernBoundary(t *testing.T) {
 	request, moves := setup(Coord{X: 5, Y: 0})
 	HandleBoundaries(request, &moves)
-
-	if Contains(moves, "down") {
-		t.Errorf("TestWhenSnakeIsAtNorthernBoundary failed, expected not to find 'down', got %v", moves)
-	} else {
-		t.Log("TestWhenSnakeIsAtNorthernBoundary passed")
-	}
+	expected := []string{"up", "left", "right"}
+	assert.Equal(t, expected, moves)
 }
 
 func TestWhenSnakeIsAtEasternBoundary(t *testing.T) {
 	request, moves := setup(Coord{X: 10, Y: 9})
 	HandleBoundaries(request, &moves)
-
-	if Contains(moves, "right") {
-		t.Errorf("TestWhenSnakeIsAtNorthernBoundary failed, expected not to find 'right', got %v", moves)
-	} else {
-		t.Log("TestWhenSnakeIsAtNorthernBoundary passed")
-	}
+	expected := []string{"up", "down", "left"}
+	assert.Equal(t, expected, moves)
 }
 
 func TestWhenSnakeIsAtWesternBoundary(t *testing.T) {
 	request, moves := setup(Coord{X: 0, Y: 5})
 	HandleBoundaries(request, &moves)
-
-	if Contains(moves, "left") {
-		t.Errorf("TestWhenSnakeIsAtNorthernBoundary failed, expected not to find 'left', got %v", moves)
-	} else {
-		t.Log("TestWhenSnakeIsAtNorthernBoundary passed")
-	}
+	expected := []string{"up", "down", "right"}
+	assert.Equal(t, expected, moves)
 }
 
 func TestHandleBoundariesWhenSnakeIsAtNorthEastCorner(t *testing.T) {
 	request, moves := setup(Coord{X: 10, Y: 10})
 	HandleBoundaries(request, &moves)
+	expected := []string{"down", "left"}
+	assert.Equal(t, expected, moves)
+}
 
-	if Contains(moves, "up") || Contains(moves, "right") {
-		t.Errorf("TestWhenSnakeIsAtNorthernBoundary failed, expected not to find 'up' or 'right', got %v", moves)
-	} else {
-		t.Log("TestWhenSnakeIsAtNorthernBoundary passed")
-	}
+func TestHandleBoundariesWhenSnakeIsAtNorthWestCorner(t *testing.T) {
+	request, moves := setup(Coord{X: 0, Y: 10})
+	HandleBoundaries(request, &moves)
+	expected := []string{"down", "right"}
+	assert.Equal(t, expected, moves)
+}
+
+func TestHandleBoundariesWhenSnakeIsAtSouthWestCorner(t *testing.T) {
+	request, moves := setup(Coord{X: 0, Y: 10})
+	HandleBoundaries(request, &moves)
+	expected := []string{"up", "right"}
+	assert.Equal(t, expected, moves)
+}
+
+func TestHandleBoundariesWhenSnakeIsAtSouthEastCorner(t *testing.T) {
+	request, moves := setup(Coord{X: 10, Y: 10})
+	HandleBoundaries(request, &moves)
+	expected := []string{"up", "left"}
+	assert.Equal(t, expected, moves)
 }
