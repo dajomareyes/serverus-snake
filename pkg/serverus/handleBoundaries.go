48e7597733c5876323c237b8a4495b80937c9fcc
David Joshua Reyes <davidjoshuareyes@gmail.com>
2021-07-04 13:25:16 -0400

    Refactor handle boundaries tests

--- pkg/serverus/handleBoundaries.go
+++ pkg/serverus/handleBoundaries.go
@@ -3,28 +3,31 @@ package serverus
 // HandleBoundaries removes illegal moves when the next move interacts with a boundary
 // Uses the current head location x,y and the boards limits to determine if the next move is possible
 // It does not return anything
-func HandleBoundaries(gameState GameRequest, possibleMoves *[]string) {
+func HandleBoundaries(gameState GameRequest, moves []string) []string {
 
 	// board dimensions should be offset by -1 to because of 0 counting
 	boardWidth := gameState.Board.Width - 1
 	boardHeight := gameState.Board.Height - 1
 	head := gameState.You.Head
+	possibleMoves := moves
 
 	if isAtNorthernBoundary(head, boardHeight) {
-		removeMoveOption(possibleMoves, "up")
+		removeMoveOption(&possibleMoves, "up")
 	}
 
 	if isAtEasternBoundary(head, boardWidth) {
-		removeMoveOption(possibleMoves, "right")
+		removeMoveOption(&possibleMoves, "right")
 	}
 
 	if isAtSouthernBoundary(head) {
-		removeMoveOption(possibleMoves, "down")
+		removeMoveOption(&possibleMoves, "down")
 	}
 
 	if isAtWesternBoundary(head) {
-		removeMoveOption(possibleMoves, "left")
+		removeMoveOption(&possibleMoves, "left")
 	}
+
+	return possibleMoves
 }
 
 func isAtNorthernBoundary(head Coord, boardHeight int) bool {
--- pkg/serverus_test/handleBoundaries_test.go
+++ pkg/serverus_test/handleBoundaries_test.go
@@ -1,6 +1,7 @@
 package serverus_test
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/dajomareyes/serverus-snake/pkg/serverus"
@@ -13,58 +14,59 @@ func setup(snakeHead Coord) (GameRequest, []string) {
 	return GameRequest{Board: BOARD_DIMS, You: Battlesnake{Head: snakeHead}}, []string{"up", "down", "left", "right"}
 }
 
-func TestWhenSnakeIsAtNorthernBoundary(t *testing.T) {
-	request, moves := setup(Coord{X: 5, Y: 10})
-	HandleBoundaries(request, &moves)
-	expected := []string{"down", "left", "right"}
-	assert.Equal(t, expected, moves)
-}
+func TestHandleBoundaries(t *testing.T) {
+	var boundaryTests = []struct {
+		title    string
+		head     Coord
+		expected []string
+	}{
+		{
+			title:    "test when snake is at the northen boundary",
+			head:     Coord{X: 4, Y: 9},
+			expected: []string{"down", "left", "right"},
+		},
+		{
+			title:    "test when snake is at the southern boundary",
+			head:     Coord{X: 4, Y: 0},
+			expected: []string{"up", "left", "right"},
+		},
+		{
+			title:    "test when snake is at the eastern boundary",
+			head:     Coord{X: 9, Y: 4},
+			expected: []string{"up", "down", "left"},
+		},
+		{
+			title:    "test when snake is at the western boundary",
+			head:     Coord{X: 0, Y: 4},
+			expected: []string{"up", "down", "right"},
+		},
+		{
+			title:    "test when snake is at the northeast corner",
+			head:     Coord{X: 9, Y: 9},
+			expected: []string{"down", "left"},
+		},
+		{
+			title:    "test when snake is at the northwest corner",
+			head:     Coord{X: 0, Y: 9},
+			expected: []string{"down", "right"},
+		},
+		{
+			title:    "test when snake is at the southeast corner",
+			head:     Coord{X: 9, Y: 0},
+			expected: []string{"up", "left"},
+		},
+		{
+			title:    "test when snake is at the southwest corner",
+			head:     Coord{X: 0, Y: 0},
+			expected: []string{"up", "right"},
+		},
+	}
 
-func TestWhenSnakeIsAtSourthernBoundary(t *testing.T) {
-	request, moves := setup(Coord{X: 5, Y: 0})
-	HandleBoundaries(request, &moves)
-	expected := []string{"up", "left", "right"}
-	assert.Equal(t, expected, moves)
-}
-
-func TestWhenSnakeIsAtEasternBoundary(t *testing.T) {
-	request, moves := setup(Coord{X: 10, Y: 9})
-	HandleBoundaries(request, &moves)
-	expected := []string{"up", "down", "left"}
-	assert.Equal(t, expected, moves)
-}
-
-func TestWhenSnakeIsAtWesternBoundary(t *testing.T) {
-	request, moves := setup(Coord{X: 0, Y: 5})
-	HandleBoundaries(request, &moves)
-	expected := []string{"up", "down", "right"}
-	assert.Equal(t, expected, moves)
-}
-
-func TestHandleBoundariesWhenSnakeIsAtNorthEastCorner(t *testing.T) {
-	request, moves := setup(Coord{X: 10, Y: 10})
-	HandleBoundaries(request, &moves)
-	expected := []string{"down", "left"}
-	assert.Equal(t, expected, moves)
-}
-
-func TestHandleBoundariesWhenSnakeIsAtNorthWestCorner(t *testing.T) {
-	request, moves := setup(Coord{X: 0, Y: 10})
-	HandleBoundaries(request, &moves)
-	expected := []string{"down", "right"}
-	assert.Equal(t, expected, moves)
-}
-
-func TestHandleBoundariesWhenSnakeIsAtSouthWestCorner(t *testing.T) {
-	request, moves := setup(Coord{X: 0, Y: 10})
-	HandleBoundaries(request, &moves)
-	expected := []string{"up", "right"}
-	assert.Equal(t, expected, moves)
-}
-
-func TestHandleBoundariesWhenSnakeIsAtSouthEastCorner(t *testing.T) {
-	request, moves := setup(Coord{X: 10, Y: 10})
-	HandleBoundaries(request, &moves)
-	expected := []string{"up", "left"}
-	assert.Equal(t, expected, moves)
+	for _, tt := range boundaryTests {
+		t.Run(fmt.Sprint(tt.title), func(t *testing.T) {
+			t.Parallel()
+			result := HandleBoundaries(setup(tt.head))
+			assert.Equal(t, tt.expected, result)
+		})
+	}
 }
