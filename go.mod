60ec71da13d1d799b08fff6947ba753415d43505
David Joshua Reyes <davidjoshuareyes@gmail.com>
2022-03-27 10:01:05 -0400

    refactor my logging

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/BattlesnakeOfficial/rules v1.0.17 // indirect
+	github.com/canthefason/go-watcher v0.2.4 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 )
--- pkg/serverus/handleObstacle.go
+++ pkg/serverus/handleObstacle.go
@@ -5,7 +5,7 @@ var DOWN Coord = Coord{0, -1}
 var LEFT Coord = Coord{-1, 0}
 var RIGHT Coord = Coord{1, 0}
 
-// returns a list of possible moves
+// HandleObstacle returns a list of possible moves
 func HandleObstacle(gameState GameRequest, possibleMoves []string) []string {
 	// splice snake body to exclude the head
 	var candidatesForRemoval []string
@@ -21,13 +21,9 @@ func HandleObstacle(gameState GameRequest, possibleMoves []string) []string {
 	return possibleMoves
 }
 
-// TODO: Implement this function, since we are also in the list of battlesnake
-// it might make sense to just get rid of isObstacleSelf and just do
-// isObstacleSnake
 func isObstacleSnake(move string, head Coord, snakes []Battlesnake) bool {
-  // loop through all the 
   var direction Coord
-  
+
   switch move {
   case "up":
     direction = UP
@@ -44,8 +40,8 @@ func isObstacleSnake(move string, head Coord, snakes []Battlesnake) bool {
   // for every battle snake in the game make sure the next move doesn't run
   // into a part of their snake
   for _, snake := range snakes {
-    for _, coordinate := range snake.Body {
-      if nextMove == coordinate {
+    for _, snakeBodyCoordinate := range snake.Body {
+      if nextMove == snakeBodyCoordinate {
         // exit out of this for loop this move won't work
         return true
       }
